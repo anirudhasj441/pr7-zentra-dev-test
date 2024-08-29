fb2e47e3f8978ba11bc01695c525363a187439a0
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-08-29 23:09:19 +0530

    add model for intrest request

--- backend/src/app/admin.py
+++ backend/src/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import IntrestRequest
 # Register your models here.
+
+class IntrestRequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "request_from",
+        "request_to",
+        "status"
+    ]
+    
+admin.site.register(IntrestRequest, IntrestRequestAdmin)
--- backend/src/app/models.py
+++ backend/src/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class IntrestRequest(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "PENDING"),
+        ("accept", "ACCEPT"),
+        ("reject", "REJECT")
+    )
+    dt = models.DateTimeField(default=timezone.now)
+    request_from = models.ForeignKey(User, on_delete=models.CASCADE, related_name="request_from")
+    request_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name="request_to")
+    status = models.CharField(max_length=1000, choices=STATUS_CHOICES, default="pending")
+    
