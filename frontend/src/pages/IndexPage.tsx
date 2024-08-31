a88ff682314838e1f5a9eb03a299a19925623b3d
Anirudha Jadhav <anirudhasj441@gmail.com>
2024-08-31 23:37:32 +0530

    dev: add base template for index page

--- frontend/src/pages/IndexPage.tsx
+++ frontend/src/pages/IndexPage.tsx
@@ -1,11 +1,23 @@
+import { Typography, Paper } from "@mui/material";
 import React from "react";
 import { memo } from "react";
 
 const IndexPage: React.FC = () => {
     return (
         <>
-            <div className="h-full pt-2">
-                <h1 className="text-center text-2xl font-bold">Index page</h1>
+            <div className="h-full w-full flex">
+                <Paper
+                    elevation={3}
+                    square
+                    className=" w-[350px] max-w-full h-full flex flex-col"
+                >
+                    <Typography variant="h4" align="center" className="py-5">
+                        Chats
+                    </Typography>
+                    <div className="flex-grow"></div>
+                    <div className="footer"></div>
+                </Paper>
+                <div className="flex-grow"></div>
             </div>
         </>
     );
